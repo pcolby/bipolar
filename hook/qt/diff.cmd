97d4c5df91df0d87d4b60f5cf90872a081bc037a
Paul Colby <git@colby.id.au>
2014-06-29 09:54:50 +1000

    Corrected the previously-reverse patch, and added to the build script.

--- hook/qt/diff.cmd
+++ hook/qt/diff.cmd
@@ -10,8 +10,12 @@
 )
 @set QT_VERSION=5.1.1
 @set SRC_DIR=qt-everywhere-opensource-src-%QT_VERSION%\qtbase\src\network\access
+ren "%SRC_DIR%\qnetworkaccessmanager.cpp" "qnetworkaccessmanager.new"
+copy "%SRC_DIR%\qnetworkaccessmanager.ori" "%SRC_DIR%\qnetworkaccessmanager.cpp"
 %DIFF% -u^
- "%SRC_DIR%\qnetworkaccessmanager.ori"^
  "%SRC_DIR%\qnetworkaccessmanager.cpp"^
+ "%SRC_DIR%\qnetworkaccessmanager.new"^
  > %~dp0\qnetworkaccessmanager.patch
+del "%SRC_DIR%\qnetworkaccessmanager.cpp"
+ren "%SRC_DIR%\qnetworkaccessmanager.new" "qnetworkaccessmanager.cpp"
 @pause
