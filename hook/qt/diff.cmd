443f30dc2d8d8f8e17d58e6176aa9511a48b69be
Paul Colby <git@colby.id.au>
2014-06-29 14:44:01 +1000

    Small grammar correction.

--- hook/qt/diff.cmd
+++ hook/qt/diff.cmd
@@ -3,11 +3,10 @@
 ) else if exist "%PROGRAMFILES(X86)%\Git\bin\diff.exe" (
   set DIFF="%PROGRAMFILES(X86)%\Git\bin\diff.exe"
 ) else (
-  echo Fail to find diff.exe
+  echo Failed to find diff.exe
   pause
   exit 1
 )
-@set QT_VERSION=5.1.1
 @set SRC_DIR=qt-everywhere-opensource-src-%QT_VERSION%\qtbase\src\network\access
 ren "%SRC_DIR%\qnetworkaccessmanager.cpp" "qnetworkaccessmanager.new"
 copy "%SRC_DIR%\qnetworkaccessmanager.ori" "%SRC_DIR%\qnetworkaccessmanager.cpp"
