ee8a579efbce21328ff26725087f2ed414bc7e4b
Paul Colby <git@colby.id.au>
2014-07-06 13:27:37 +1000

    Some double samples (latitude and longitude) from FlowSync data.

--- test/protobuf/testfixnum.cpp
+++ test/protobuf/testfixnum.cpp
@@ -42,7 +42,15 @@ void TestFixnum::parseDouble_data()
         << QByteArray("\x0B\x0B\xEE\x07\x3C\xDD\x5E\xC0")
         << QVariant(-123.456789);
 
-    /// @todo  Add some examples from actual FlowSync data too.
+    // Latitude samples from actual FlowSync data.
+    QTest::newRow("mzoo:route:2:1:1")
+        << QByteArray("\342es\302q\344B\300")
+        << QVariant(-37.784721666666667);
+
+    // Longitude samples from actual FlowSync data.
+    QTest::newRow("mzoo:route:3:1:1")
+        << QByteArray("\242\336y\317n\036b@")
+        << QVariant(144.951026666666667);
 }
 
 void TestFixnum::parseDouble()
@@ -68,7 +76,40 @@ void TestFixnum::parseDoubles_data()
                       "\x0B\x0B\xEE\x07\x3C\xDD\x5E\xC0", 24)
         << list;
 
-    /// @todo  Add some examples from actual FlowSync data too.
+    // Latitude samples from actual FlowSync data.
+    list.clear();
+    list << -37.7847216666666667 << -37.7847216666666667 << -37.7847416666666667
+         << -37.7847533333333333 << -37.7847583333333333 << -37.7847633333333333
+         << -37.78477            << -37.784775           << -37.78478
+         << -37.784785           << -37.7847916666666667 << -37.7847983333333333
+         << -37.7848033333333333 << -37.7848116666666667 << -37.7848183333333333;
+    QTest::newRow("mzoo:route:2:1")
+        << QByteArray("\342es\302q\344B\300\342es\302q\344B\300"
+                      ")\0229jr\344B\300\375\013\027\314r\344B\300"
+                      "\017w\010\366r\344B\300 \342\371\037s\344B\300"
+                      "\343p\346Ws\344B\300\365\333\327\201s\344B\300"
+                      "\006G\311\253s\344B\300\030\262\272\325s\344B\300"
+                      "\332@\247\rt\344B\300\235\317\223Et\344B\300"
+                      "\257:\205ot\344B\300\"\355l\265t\344B\300"
+                      "\344{Y\355t\344B\300")
+        << list;
+
+    // Longitude samples from actual FlowSync data.
+    list.clear();
+    list << 144.951026666666667 << 144.951026666666667 << 144.951021666666667
+         << 144.951011666666667 << 144.951001666666667 << 144.95099
+         << 144.950976666666667 << 144.95096           << 144.950945
+         << 144.95093           << 144.950913333333333 << 144.950898333333333
+         << 144.950883333333333 << 144.95087           << 144.950858333333333;
+    QTest::newRow("mzoo:route:3:1")
+        << QByteArray("\242\336y\317n\036b@\242\336y\317n\036b@"
+                      "\335\203\375\304n\036b@T\316\004\260n\036b@"
+                      "\313\030\014\233n\036b@V\232\224\202n\036b@"
+                      "\365R\236fn\036b@\274y\252Cn\036b@ni5$n\036b"
+                      "@!Y\300\004n\036b@\350\177\314\341m\036b@"
+                      "\232oW\302m\036b@M_\342\242m\036b"
+                      "@\354\027\354\206m\036b@w\231tnm\036b@")
+        << list;
 }
 
 void TestFixnum::parseDoubles()
