127347eceebcf739af00c8ff247115335cfd06ed
Paul Colby <git@colby.id.au>
2014-07-23 17:29:50 +1000

    Explore Travis CI unit test error

--- src/polar/v2/trainingsession.cpp
+++ src/polar/v2/trainingsession.cpp
@@ -865,6 +865,8 @@ QDomDocument TrainingSession::toTCX(const QString &buildTime) const
                         .appendChild(doc.createTextNode(cadence.at(index).toString()));
                 }
 
+                qDebug() << __FUNCTION__ << __LINE__ << startTime;
+                qDebug() << __FUNCTION__ << __LINE__ << startTime.toString(Qt::ISODate);
                 qDebug() << __FUNCTION__ << __LINE__ << startTime.addMSecs(index * recordInterval);
                 qDebug() << __FUNCTION__ << __LINE__ << startTime.addMSecs(index * recordInterval).toString(Qt::ISODate);
                 if (trackPoint.hasChildNodes()) {
