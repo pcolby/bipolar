57c375eb987fe6d6e506565d5eb4b3166897b375
Paul Colby <git@colby.id.au>
2014-07-31 21:43:31 +1000

    Implement OSX hook patching

--- hook/qt/build.sh
+++ hook/qt/build.sh
@@ -3,8 +3,10 @@
 QT_VERSION=5.1.1 # The version used by Polar FlowSync.
 QT_NAME="qt-everywhere-opensource-src-$QT_VERSION"
 
+CP=`which cp`       || { echo 'Failed to find: cp'    2>&1; exit 1; }
 CURL=`which curl`   || { echo 'Failed to find: curl'  2>&1; exit 1; }
 PATCH=`which patch` || { echo 'Failed to find: patch' 2>&1; exit 1; }
+SED=`which sed`     || { echo 'Failed to find: sed'   2>&1; exit 1; }
 TAR=`which tar`     || { echo 'Failed to find: tar'   2>&1; exit 1; }
 
 SELF_DIR=`dirname "$0"`
@@ -23,6 +25,7 @@ function fetchSource {
 function extractSource {
     if [ ! -e "$SELF_DIR/$QT_NAME" ]; then
         fetchSource || return
+        echo "Extracting $QT_NAME.tar.gz"
         "$TAR" xzf "$SELF_DIR/$QT_NAME.tar.gz" -C "$SELF_DIR" || return
     fi
 }
@@ -30,13 +33,29 @@ function extractSource {
 # Patch the source with our hook code.
 function patchSource {
     extractSource || return
-    # @todo "$PATCH" ...
+    NETWORK_ACCESS_DIR="$SELF_DIR/$QT_NAME/qtbase/src/network/access/"
+    if [ ! -e "$NETWORK_ACCESS_DIR/qnetworkaccessmanager.ori" ]; then
+        echo "Backing up $NETWORK_ACCESS_DIR/qnetworkaccessmanager.cpp"
+        "$CP" -a \
+            "$NETWORK_ACCESS_DIR/qnetworkaccessmanager.cpp" \
+            "$NETWORK_ACCESS_DIR/qnetworkaccessmanager.ori"
+        if [ $? -ne 0 ]; then return; fi
+    fi
+    echo "Applying qnetworkaccessmanager.patch"
+    "$SED" -e '1,2 s/\\/\//g' "$SELF_DIR/qnetworkaccessmanager.patch" | \
+        "$PATCH" --forward --strip 0
+    if [ $? -eq 1 ]; then
+        echo 'Assuming patch is already applied and continuing.'
+    fi
 }
 
 # Configure the Qt build.
 function configure {
     patchSource || return
-    # @todo configure
+    pushd "$SELF_DIR/$QT_NAME"
+    # @todo Do we need to build 32 or 64-bit? (depends on what FlowSync uses).
+    echo ./configure
+    popd
 }
 
 # Build Qt.
