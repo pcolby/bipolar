1b6945aa5fa9626a5880d98a250034cd7d412404
Paul Colby <git@colby.id.au>
2014-08-09 14:20:39 +1000

    Round some numbers ever so slightly better

--- src/polar/v2/trainingsession.cpp
+++ src/polar/v2/trainingsession.cpp
@@ -1421,7 +1421,7 @@ QStringList TrainingSession::toHRM() const
                 summaryThRow1[4]++;
         }
         stream << "\r\n[Summary-TH]\r\n"; // WebSync includes 0's when empty.
-        stream << (heartrate.length() * qRound(recordInterval/1000.0f));
+        stream << qRound(heartrate.length() * recordInterval / 1000.0);
         for (size_t index = 0; index < (sizeof(summaryThRow1)/sizeof(summaryThRow1[0])); ++index) {
             stream << '\t' << qRound(summaryThRow1[index] * recordInterval / 1000.0f);
         }
