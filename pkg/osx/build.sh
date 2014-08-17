76a8d68c60d4236dab973f072b46b7883bc0f073
Paul Colby <git@colby.id.au>
2014-08-17 20:17:11 +1000

    Bump the version to 0.3.0 (the GUI's getting close)

--- pkg/osx/build.sh
+++ pkg/osx/build.sh
@@ -52,7 +52,7 @@ cp README.txt /Volumes/Bipolar/  || exit
 hdiutil detach /Volumes/Bipolar/ || exit
 
 echo 'Converting final disk image'
-FINALNAME="Bipolar-0.2.4.$REVISION.dmg"
+FINALNAME="Bipolar-0.3.0.$REVISION.dmg"
 [ -e "$FINALNAME" ] && rm -f "$FINALNAME"
 hdiutil convert Bipolar-rw.dmg -format UDZO -o "$FINALNAME"
 
