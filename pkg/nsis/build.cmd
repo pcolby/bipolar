fbf3bbfbba9a945d576c3adf7c1801d18886577e
Paul Colby <git@colby.id.au>
2015-08-17 16:02:49 +1000

    Don't sed the win32.nsi script anymore

--- pkg/nsis/build.cmd
+++ pkg/nsis/build.cmd
@@ -2,7 +2,6 @@
 
 :: Make sure the NSIS, git and Qt binaries are in the path.
 path=%path%;C:\Program Files\NSIS;C:\Program Files (x86)\NSIS
-path=%path%;C:\Program Files\Git\bin;C:\Program Files (x86)\Git\bin
 path=%path%;C:\Qt\5.3\msvc2013\bin
 
 :: Gather the Qt dependencies.
@@ -14,13 +13,6 @@ if errorlevel 1 (
   exit errorlevel
 )
 
-:: Set the REVISION variable to the current git revision number.
-git.exe rev-list --count HEAD > .revision
-set /p REVISION=<.revision
-del .revision
-
-:: Update the NSIS script, and build the installer.
-sed.exe -e "s/\(\([ 0-9]\+[.,]\)\{3\} *\)0/\1%REVISION%/g" win32.nsi.in > win32.nsi
 makensis win32.nsi
 
 pause
