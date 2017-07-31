d9b652a4c9b47312171d23265f4b68a8c11db792
Paul Colby <git@colby.id.au>
2017-07-31 14:08:28 +1000

    Remove unnecessary $ from arithmetic variable
    As reported by Codacy. Good practice, really.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -6,6 +6,6 @@ for platform in unix{32,64} win{32{A,W},64}; do
            -I{src,src/{polar/v2,protobuf,threads,widgets},test} \
            --platform=$platform --suppress={missingIncludeSystem,unusedFunction} \
            --std={c++03,c++11,posix} --quiet .
-  RC=$(($RC+$?))
+  RC=$((RC+$?))
 done
 exit $RC
