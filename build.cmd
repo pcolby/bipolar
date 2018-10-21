94f72a03b9af2e3227a5629c7a371a8e80b8699c
Paul Colby <git@colby.id.au>
2018-10-21 14:01:32 +1100

    Build the Windows hook with Visual Studio 2013 #89

--- build.cmd
+++ build.cmd
@@ -2,13 +2,13 @@
 :: Prerequisites:
 :: * MSVC tool chain; ideally version 10.0, such as Visual C++ Express 2010.
 ::    * Installed.
-:: * OpenSSL (ideally version 1.0.1g, or ABI compatible), headers and binaries installed.
-::    * http://slproweb.com/download/Win32OpenSSL-1_0_1g.exe
+:: * OpenSSL (ideally version 1.0.2o, or ABI compatible), headers and binaries installed.
+::    * http://slproweb.com/download/Win32OpenSSL-1_0_2p.exe
 :: * Qt 5.5.1 source, in the "src" subdirectory.
 ::    * http://download.qt.io/archive/qt/5.5/5.5.1/single/
 
 :: The following are all required; adjust to match your setup.
-set MSVC=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
+set MSVC=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
 set OPENSSL=C:\OpenSSL-Win32
 set PATCH=C:\Program Files\Git\usr\bin\patch.exe
 set QT_VERSION=5.5.1
