8fe0fa983296d848c88bb8fea2e7363fea25b437
Paul Colby <git@colby.id.au>
2019-03-15 20:17:52 +1100

    Add some comments on (probably) redundant commands

--- install.sh
+++ install.sh
@@ -23,12 +23,20 @@ echo 'Installing hook'
 cp build/qtbase/lib/QtNetwork.framework/Versions/5/QtNetwork \
    "$TARGET_APP/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork"
 
+# @todo Consider removing this.  Current Qt/OSX buildchain set the ID to
+# '@rpath/QtNetwork.framework/Versions/5/QtNetwork', which should work fine.
+# Whereas back in 2014 it would typically be set to an absolute path on the
+# build machine. Anyway, forcing it to be based on '@executable_path/...'
+# still seems to work, but we can probably drop it sometime, with testing.
 install_name_tool -id \
     '@executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork' \
     "$TARGET_APP/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork"
 
+# @todo Consider removing this.  It actually does nothing now, since current Qt
+# versions set the relevant name to '@rpath/QtCore.framework/Versions/5/QtCore'
+# so the following `-change` doesn't find anything to change anymore.
 install_name_tool -change \
-    '/usr/local/Qt-5.1.1/lib/QtCore.framework/Versions/5/QtCore' \
+    '/usr/local/Qt-5.5.1/lib/QtCore.framework/Versions/5/QtCore' \
     '@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore' \
     "$TARGET_APP/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork"
 
