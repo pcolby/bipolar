95d92224cdca1faf3deb7c610a30a0eb28d7a23e
Paul Colby <git@colby.id.au>
2023-04-14 21:51:10 +1000

    Add some more muscle to the .cppcheck.sh script

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -2,13 +2,15 @@
 # SPDX-FileCopyrightText: 2014-2019 Paul Colby <git@colby.id.au>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+cppcheck --version
 RC=0
-for platform in unix{32,64} win{32{A,W},64}; do
+for platform in native unix{32,64} win{32{A,W},64} unspecified; do
   echo "$platform:"
   cppcheck -DBUILD_USER=test --enable=all --error-exitcode=1 \
            -I{src,src/{polar/v2,protobuf,threads,widgets},test} \
-           --platform=$platform --suppress={missingIncludeSystem,unusedFunction} \
-           --std=c++{03,11,14,17,20} --quiet .
+           --language=c++ --platform="$platform" --std=c++{03,11,14,17,20} \
+           --suppress={missingIncludeSystem,unusedFunction} \
+           --quiet "$@" src test | grep -Eve '^Checking' | uniq --skip-fields=1
   RC=$((RC+$?))
 done
 exit $RC
