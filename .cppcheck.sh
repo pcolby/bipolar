5a5270bee19f4b2aba2a5d4dcc2b5a03835653a9
Paul Colby <git@colby.id.au>
2023-04-14 21:46:23 +1000

    Stop using the (removed from cppcheck) posix standard
    And include the more recent C++ standards too.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -8,7 +8,7 @@ for platform in unix{32,64} win{32{A,W},64}; do
   cppcheck -DBUILD_USER=test --enable=all --error-exitcode=1 \
            -I{src,src/{polar/v2,protobuf,threads,widgets},test} \
            --platform=$platform --suppress={missingIncludeSystem,unusedFunction} \
-           --std={c++03,c++11,posix} --quiet .
+           --std=c++{03,11,14,17,20} --quiet .
   RC=$((RC+$?))
 done
 exit $RC
